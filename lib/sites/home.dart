8d1f3b633d56364f1ac28c6e4caaa91e38427c08
Lin2D2 <linus@thebehrens.net>
2020-09-11 23:36:51 +0200

    whole rewrite of home

--- lib/sites/home.dart
+++ lib/sites/home.dart
@@ -1,193 +1,161 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  Home({Key key}) : super(key: key);
-
   @override
-  _HomeState createState() => new _HomeState();
+  State createState() => new DyanmicList();
 }
 
-class _HomeState extends State<Home> {
+class DyanmicList extends State<Home> {
+  TableRow ElementsToADD = TableRow(children: []);
   @override
-  Widget build(BuildContext context) {
-    return _buildList(context);
-  }
-
-  Widget _buildList(BuildContext context) {
-    List<Widget> _ListItems = <Widget>[
-      Card(
-          elevation: 5.0,
-          child: Center(
-              child: Padding(
-            padding: EdgeInsets.all(4.0),
-            child: Text(
-              "Dashboard",
-              style: new TextStyle(
-                fontSize: 40.0,
-                color: Colors.black,
+  Widget build(BuildContext ctxt) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Card(
+                elevation: 5.0,
+                child: Center(
+                    child: Padding(
+                  padding: EdgeInsets.all(4.0),
+                  child: Text(
+                    "Dashboard",
+                    style: new TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ))),
+            Expanded(
+              child: ListView(
+                children: <Card>[
+                  CreateItem(
+                      "Overview Today",
+                      [
+                        CreateTableRow(
+                          "1/2",
+                          "MA",
+                          "C5",
+                          false,
+                          ExtraColumn: "room C6",
+                        ),
+                        CreateTableRow(
+                          "3/4",
+                          "de",
+                          "C7",
+                          false,
+                          ExtraColumn: "vertretung",
+                        ),
+                      ],
+                      ExtraColumn: "change"),
+                  CreateItem(
+                    "Time Table Today",
+                    [
+                      CreateTableRow(
+                        " ",
+                        " ",
+                        " ",
+                        false,
+                      ),
+                    ],
+                  ),
+                  CreateItem(
+                      "Overview Tomorow",
+                      [
+                        CreateTableRow(
+                          " ",
+                          " ",
+                          " ",
+                          false,
+                          ExtraColumn: " ",
+                        ),
+                      ],
+                      ExtraColumn: "change"),
+                  CreateItem("Time Table Tomorow", [
+                    CreateTableRow(
+                      " ",
+                      " ",
+                      " ",
+                      false,
+                    ),
+                  ]),
+                ],
               ),
             ),
-          ))),
-      Column(children: <Widget>[
-        create_overview("Overview Today", true, [
-          // Demo Content
-          [
-            "1/2",
-            "Ma",
-            "C5",
-            "room changed",
           ],
-          [
-            "3/4",
-            "DE",
-            "C7",
-            "room changed",
-          ],
-        ]),
-        create_overview("Time Table Today", false, [
-          // Demo Content
-          [
-            "1/2",
-            "Ma",
-            "C5",
-          ],
-          [
-            "3/4",
-            "DE",
-            "C7",
-          ],
-          [
-            "5/6",
-            "Ma",
-            "C5",
-          ],
-          [
-            "7/8",
-            "DE",
-            "C7",
-          ],
-        ]),
-        create_overview("Overview Tomorow", true, [
-          // Demo Content
-          [
-            "3/4",
-            "DE",
-            "C7",
-            "room changed",
-          ],
-        ]),
-        create_overview("Time Table Tomorow", false, [
-          // Demo Content
-          [
-            "1/2",
-            "Ma",
-            "C5",
-          ],
-          [
-            "3/4",
-            "DE",
-            "C7",
-          ],
-          [
-            "5",
-            "Ma",
-            "C5",
-          ],
-          [
-            "6",
-            "DE",
-            "C7",
-          ],
-          [
-            "7/8",
-            "Ma",
-            "C5",
-          ],
-          [
-            "9",
-            "DE",
-            "C7",
-          ],
-        ]),
-      ])
-    ];
-    return Scaffold(
-      body: ListView.builder(
-          itemCount: _ListItems.length,
-          itemBuilder: (BuildContext context, int index) {
-            return (_ListItems[index]);
-          }),
+        ),
+      ),
     );
   }
 }
 
-Widget create_overview(String type, bool change, List<List<String>> happend) {
-  const TextStyle optionStyle =
-      TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
-  const TextStyle secoundoptionStyle =
-      TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: Colors.white);
-  List<TableRow> rows = [];
-  for (var item in happend) {
-    List<Text> row_elements = [];
-    for (String element in item) {
-      row_elements.add(Text(
-        element,
-        textAlign: TextAlign.center,
+Card CreateItem(String Title, List<TableRow> TableRows,
+    {String ExtraColumn = ""}) {
+  TableRows.insert(
+      0,
+      CreateTableRow(
+        "lesson",
+        "subject",
+        "room",
+        true,
+        ExtraColumn: ExtraColumn,
       ));
-    }
-    rows.add(TableRow(children: row_elements));
-  }
-  List<Text> header = [
-    Text(
-      "lesson",
-      textAlign: TextAlign.center,
-      style: secoundoptionStyle,
+  return Card(
+    color: Colors.white30,
+    elevation: 5.0,
+    child: Column(
+      children: [
+        Center(
+          child: Text(
+            Title,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Table(
+            border: TableBorder.all(),
+            children: TableRows,
+          ),
+        ),
+      ],
     ),
-    Text(
-      "subject",
+  );
+}
+
+TableRow CreateTableRow(String item1, String item2, String item3, bool Header,
+    {String ExtraColumn = ""}) {
+  TextStyle Style = TextStyle(
+      fontSize: 13,
+      fontWeight: FontWeight.bold,
+      color: (Header ? Colors.white : Colors.black));
+  TableRow Row = TableRow(
+      decoration: BoxDecoration(
+        color: (Header ? Colors.grey[900] : Colors.white),
+      ),
+      children: [
+        Text(
+          item1,
+          textAlign: TextAlign.center,
+          style: Style,
+        ),
+        Text(
+          item2,
+          textAlign: TextAlign.center,
+          style: Style,
+        ),
+        Text(
+          item3,
+          textAlign: TextAlign.center,
+          style: Style,
+        ),
+      ]);
+  if (ExtraColumn != "") {
+    Row.children.add(Text(
+      ExtraColumn,
       textAlign: TextAlign.center,
-      style: secoundoptionStyle,
-    ),
-    Text(
-      "room",
-      textAlign: TextAlign.center,
-      style: secoundoptionStyle,
-    ),
-  ];
-  if (change) {
-    header.add(Text(
-      "change",
-      textAlign: TextAlign.center,
-      style: secoundoptionStyle,
+      style: Style,
     ));
   }
-
-  rows.insert(
-      0,
-      TableRow(
-        children: header,
-        decoration: BoxDecoration(
-          color: Colors.grey[900],
-        ),
-      ));
-
-  return Card(
-      color: Colors.white30,
-      elevation: 5.0,
-      child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Center(
-                child: Text(
-                  type,
-                  style: optionStyle,
-                ),
-              ),
-              Table(
-                border: TableBorder.all(),
-                children: rows,
-              ),
-            ],
-          )));
+  return Row;
 }
