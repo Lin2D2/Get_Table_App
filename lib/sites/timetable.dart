a926c454556cee5c3cdb02d7fcd7206151d54aea
space <linus@thebehrens.net>
2020-09-18 00:22:30 +0200

    rewrote and added Inexed Stack and Appbar

--- lib/sites/timetable.dart
+++ lib/sites/timetable.dart
@@ -6,6 +6,7 @@ class TimeTable extends StatefulWidget {
 }
 
 class DynamicList extends State<TimeTable> {
+  int _selectedIndex = 0;
   List<TableRow> monday = [
     headerRow(),
   ];
@@ -21,310 +22,229 @@ class DynamicList extends State<TimeTable> {
   List<TableRow> friday = [
     headerRow(),
   ];
-  TableRow elementsToADD = TableRow(children: [
-    Container(
+  TableRow elementsToADD = TableRow(
+    children: [
+      Container(
         height: 30,
         child: TextField(
-            textAlign: TextAlign.center,
-            style:
-                TextStyle(fontSize: 15.0, height: 2.0, color: Colors.black))),
-    Container(
-        height: 30,
-        child: TextField(
-            textAlign: TextAlign.center,
-            style:
-                TextStyle(fontSize: 15.0, height: 2.0, color: Colors.black))),
-    Container(
-        height: 30,
-        child: TextField(
-            textAlign: TextAlign.center,
-            style:
-                TextStyle(fontSize: 15.0, height: 2.0, color: Colors.black))),
-  ]);
-  @override
-  Widget build(BuildContext ctxt) {
-    return Scaffold(
-      body: Container(
-        child: ListView(
-          children: <Widget>[
-            Card(
-                child: Column(
-              children: [
-                Center(
-                  child: Text(
-                    "Monday",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Table(
-                  border: TableBorder.all(),
-                  children: monday,
-                ),
-                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(2, 2, 2, 0),
-                    child: IconButton(
-                      // add new Row to Table
-                      onPressed: () {
-                        monday.add(elementsToADD);
-                        setState(() {});
-                      },
-                      splashRadius: 20.0,
-                      icon: Icon(
-                        Icons.add,
-                        color: Colors.pink,
-                        size: 30.0,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(2, 2, 2, 0),
-                    child: IconButton(
-                      // add new Row to Table
-                      onPressed: () {
-                        monday.removeLast();
-                        setState(() {});
-                      },
-                      splashRadius: 20.0,
-                      icon: Icon(
-                        Icons.remove,
-                        color: Colors.pink,
-                        size: 30.0,
-                      ),
-                    ),
-                  ),
-                ])
-              ],
-            )),
-            Card(
-                child: Column(
-              children: [
-                Center(
-                  child: Text(
-                    "Tuesday",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Table(
-                  border: TableBorder.all(),
-                  children: tuesday,
-                ),
-                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(2, 2, 2, 0),
-                    child: IconButton(
-                      // add new Row to Table
-                      onPressed: () {
-                        tuesday.add(elementsToADD);
-                        setState(() {});
-                      },
-                      splashRadius: 20.0,
-                      icon: Icon(
-                        Icons.add,
-                        color: Colors.pink,
-                        size: 30.0,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(2, 2, 2, 0),
-                    child: IconButton(
-                      // add new Row to Table
-                      onPressed: () {
-                        tuesday.removeLast();
-                        setState(() {});
-                      },
-                      splashRadius: 20.0,
-                      icon: Icon(
-                        Icons.remove,
-                        color: Colors.pink,
-                        size: 30.0,
-                      ),
-                    ),
-                  ),
-                ])
-              ],
-            )),
-            Card(
-                child: Column(
-              children: [
-                Center(
-                  child: Text(
-                    "Wendsday",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Table(
-                  border: TableBorder.all(),
-                  children: wednesday,
-                ),
-                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(2, 2, 2, 0),
-                    child: IconButton(
-                      // add new Row to Table
-                      onPressed: () {
-                        wednesday.add(elementsToADD);
-                        setState(() {});
-                      },
-                      splashRadius: 20.0,
-                      icon: Icon(
-                        Icons.add,
-                        color: Colors.pink,
-                        size: 30.0,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(2, 2, 2, 0),
-                    child: IconButton(
-                      // add new Row to Table
-                      onPressed: () {
-                        wednesday.removeLast();
-                        setState(() {});
-                      },
-                      splashRadius: 20.0,
-                      icon: Icon(
-                        Icons.remove,
-                        color: Colors.pink,
-                        size: 30.0,
-                      ),
-                    ),
-                  ),
-                ])
-              ],
-            )),
-            Card(
-                child: Column(
-              children: [
-                Center(
-                  child: Text(
-                    "Thursday",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Table(
-                  border: TableBorder.all(),
-                  children: thursday,
-                ),
-                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(2, 2, 2, 0),
-                    child: IconButton(
-                      // add new Row to Table
-                      onPressed: () {
-                        thursday.add(elementsToADD);
-                        setState(() {});
-                      },
-                      splashRadius: 20.0,
-                      icon: Icon(
-                        Icons.add,
-                        color: Colors.pink,
-                        size: 30.0,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(2, 2, 2, 0),
-                    child: IconButton(
-                      // add new Row to Table
-                      onPressed: () {
-                        thursday.removeLast();
-                        setState(() {});
-                      },
-                      splashRadius: 20.0,
-                      icon: Icon(
-                        Icons.remove,
-                        color: Colors.pink,
-                        size: 30.0,
-                      ),
-                    ),
-                  ),
-                ])
-              ],
-            )),
-            Card(
-              child: Column(
-                children: [
-                  Center(
-                    child: Text(
-                      "Friday",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Table(
-                    border: TableBorder.all(),
-                    //children: Days[0],
-                    children: friday,
-                  ),
-                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(2, 2, 2, 0),
-                      child: IconButton(
-                        // add new Row to Table
-                        onPressed: () {
-                          friday.add(elementsToADD);
-                          setState(() {});
-                        },
-                        splashRadius: 20.0,
-                        icon: Icon(
-                          Icons.add,
-                          color: Colors.pink,
-                          size: 30.0,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(2, 2, 2, 0),
-                      child: IconButton(
-                        // add new Row to Table
-                        onPressed: () {
-                          friday.removeLast();
-                          setState(() {});
-                        },
-                        splashRadius: 20.0,
-                        icon: Icon(
-                          Icons.remove,
-                          color: Colors.pink,
-                          size: 30.0,
-                        ),
-                      ),
-                    ),
-                  ])
-                ],
-              ),
-            ),
-          ],
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 15.0, height: 2.0, color: Colors.black),
         ),
       ),
+      Container(
+        height: 30,
+        child: TextField(
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 15.0, height: 2.0, color: Colors.black),
+        ),
+      ),
+      Container(
+        height: 30,
+        child: TextField(
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 15.0, height: 2.0, color: Colors.black),
+        ),
+      ),
+    ],
+  );
+
+  @override
+  Widget build(BuildContext ctxt) {
+    return IndexedStack(
+      index: _selectedIndex,
+      children: [
+        Scaffold(
+          body: SingleChildScrollView(
+            child: Table(),
+          ),
+          appBar: AppBar(
+            backgroundColor: Colors.grey[900],
+            leading: IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {
+                setState(() {});
+              },
+            ),
+            title: Center(
+              child: const Text(
+                'TimeTable',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text(
+                  "edit",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {
+                  setState(
+                    () {
+                      _selectedIndex = 1;
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+        Scaffold(
+          body: Container(
+            child: ListView(
+              children: List.generate(
+                  [monday, tuesday, wednesday, thursday, friday].length,
+                  (i) => Card(
+                        child: Column(
+                          children: [
+                            Center(
+                              child: Text(
+                                [
+                                  "Monday",
+                                  "Tuesday",
+                                  "Wednesday",
+                                  "Thursday",
+                                  "Friday"
+                                ][i],
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Table(
+                              border: TableBorder.all(),
+                              children: [
+                                monday,
+                                tuesday,
+                                wednesday,
+                                thursday,
+                                friday
+                              ][i],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.fromLTRB(2, 2, 2, 0),
+                                  child: IconButton(
+                                    // add new Row to Table
+                                    onPressed: () {
+                                      [
+                                        monday,
+                                        tuesday,
+                                        wednesday,
+                                        thursday,
+                                        friday
+                                      ][i]
+                                          .add(elementsToADD);
+                                      setState(() {});
+                                    },
+                                    splashRadius: 20.0,
+                                    icon: Icon(
+                                      Icons.add,
+                                      color: Colors.pink,
+                                      size: 30.0,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.fromLTRB(2, 2, 2, 0),
+                                  child: IconButton(
+                                    // add new Row to Table
+                                    onPressed: () {
+                                      [
+                                        monday,
+                                        tuesday,
+                                        wednesday,
+                                        thursday,
+                                        friday
+                                      ][i]
+                                          .removeLast();
+                                      setState(() {});
+                                    },
+                                    splashRadius: 20.0,
+                                    icon: Icon(
+                                      Icons.remove,
+                                      color: Colors.pink,
+                                      size: 30.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      )),
+            ),
+          ),
+          appBar: AppBar(
+            backgroundColor: Colors.grey[900],
+            title: Center(
+              child: const Text(
+                'Edit Timetable',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                setState(() {
+                  _selectedIndex = 0;
+                });
+              },
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text(
+                  "save",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {
+                  setState(() {});
+                },
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
 
 TableRow headerRow() {
   return TableRow(
-      decoration: BoxDecoration(
-        color: Colors.grey[900],
+    decoration: BoxDecoration(
+      color: Colors.grey[900],
+    ),
+    children: [
+      Text(
+        "lesson",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            fontSize: 13, fontWeight: FontWeight.bold, color: Colors.white),
       ),
-      children: [
-        Text(
-          "lesson",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-              fontSize: 13, fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-        Text(
-          "subject",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-              fontSize: 13, fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-        Text(
-          "room",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-              fontSize: 13, fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-      ]);
+      Text(
+        "subject",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            fontSize: 13, fontWeight: FontWeight.bold, color: Colors.white),
+      ),
+      Text(
+        "room",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            fontSize: 13, fontWeight: FontWeight.bold, color: Colors.white),
+      ),
+    ],
+  );
 }
