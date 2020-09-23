2164d2d9938266bdd6ee06a7cf0a0c3ec565b446
Lin2D2 <linus@thebehrens.net>
2020-09-23 23:34:56 +0200

    removed unused var

--- lib/widgets/weekDropdownButton.dart
+++ lib/widgets/weekDropdownButton.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class WeekDropdownButton extends StatelessWidget {
-  String dropdownValue = 'One';
-
   @override
   Widget build(BuildContext context) {
     return Center(
