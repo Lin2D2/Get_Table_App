3b28f41986a508f78fb20d3eb7386c7754ce0604
Lin2D2 <linus@thebehrens.net>
2020-12-03 21:39:13 +0100

    fixed the Coloring problem

--- lib/widgets/bottomNavigationBar.dart
+++ lib/widgets/bottomNavigationBar.dart
@@ -38,11 +38,8 @@ Widget bottomNavigationBar(BuildContext context) {
     type: BottomNavigationBarType.fixed,
     currentIndex: context.watch<IndexMainBloc>().index,
     backgroundColor: Colors.grey[900],
-    // TODO the Colors below don't apply
-    unselectedLabelStyle: TextStyle(color: Colors.white),
-    unselectedIconTheme: IconThemeData(color: Colors.white),
-    selectedLabelStyle: TextStyle(color: Colors.amber[800]),
-    selectedIconTheme: IconThemeData(color: Colors.amber[800]),
+    fixedColor: Colors.amber[800],
+    unselectedItemColor: Colors.white,
     onTap: _onItemTapped,
     items: const <BottomNavigationBarItem>[
       BottomNavigationBarItem(
